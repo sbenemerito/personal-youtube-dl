6bc1fe8d425db26d7202ee322e4a8a19f8192f01
sbenemerito <benemeritosam@gmail.com>
2017-12-14 17:11:10 +0800

    audio now mp3, now has progress bar

--- youtube_download.py
+++ youtube_download.py
@@ -4,20 +4,34 @@ import urllib.request
 import urllib.parse
 import bs4
 import re
+import os
+import sys
+from subprocess import run
+
+
+def convert_to_mp3(filename, path):
+    run(['mplayer', '-novideo', '-ao', 'pcm:waveheader', filename + '.mp4'])
+    run(['lame', '-h', 'audiodump.wav', filename + '.mp3'])
+    os.remove('audiodump.wav')
+    os.remove(filename + '.mp4')
 
 
 def show_progress_bar(stream, chunk, file_handle, bytes_remaining):
     filesize = stream.filesize
     finished = filesize - bytes_remaining
-    progress = float(finished)/filesize * 100
-    print('{0} / {1} bytes ({2: .2f}% complete)'.format(finished, filesize, progress)) 
-    print('--------')
+    progress = int(finished/filesize * 100)
+    complete = int(progress/10)
+    remaining = 10-complete
+    bar = '{}{}'.format('#'*complete, '-'*remaining)
+    sys.stdout.write('[{3}] {0} / {1} bytes ({2}% complete)\r'.format(finished, filesize, progress, bar))
+    sys.stdout.flush()
 
 
 def askForPath():
     path = input('Save to (uses relative path, leave blank for current directory): ')
-    path = None if len(path.strip()) == 0 else path
+    path = os.getcwd() if len(path.strip()) == 0 else path
     return path
+
 
 query_string = urllib.parse.urlencode({"search_query" : input('Search YouTube: ')})
 html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
@@ -26,47 +40,24 @@ results = soup.find_all("a", href=re.compile(r'^/watch'))
 results = [result for result in results if result.get('title')]
 
 for i, result in enumerate(results):
-    print('[{0}] {1} (http://youtube.com{2})'.format(i+1, result.get('title'), result.get('href')))
+    print('[{0}] {1}'.format(i+1, result.get('title')))
 
 selected_video = int(input('Select video number: '))
 if selected_video in range(1, len(results)+1):
     video_url = 'http://youtube.com' + results[selected_video-1].get('href')
     selected_video = YouTube(video_url)
     selected_video.register_on_progress_callback(show_progress_bar)
-    print(selected_video.title)
-    print('[1] Download video + audio')
-    print('[2] Download audio')
+
+    print('[1] Download video (.mp4)')
+    print('[2] Download audio only (.mp3)')
     ch = input('Option: ')
     save_path = askForPath()
     print('Downloading...')
     if ch == '1':
         selected_video.streams.filter(subtype='mp4').filter(progressive=True).all()[1].download(save_path)
-        print('Download complete')
+        print('\nDownload complete')
     elif ch == '2':
         selected_video.streams.filter(only_audio=True).filter(subtype='mp4').all()[0].download(save_path)
-        print('Download complete')
-
-
-# import requests
-
-
-# response = requests.get('https://github.com/EbookFoundation/free-programming-books/blob/master/free-programming-books.md')
-
-# print(soup.prettify())
-# linkElems = soup.select('a')
-# i = 0
-# for linkElem in linkElems:
-#   if i == 5:
-#       break
-#   if linkElem.get('href').endswith('.pdf'):
-#           print('Downloading ' + linkElem.get('href'))
-
-#           pdfUrl = linkElem.get('href')
-#           pdfFilename = pdfUrl[pdfUrl.rfind('/') + 1:]
-#           pdfFile = open(pdfFilename, 'wb')
-
-#           pdfResponse = requests.get(pdfUrl)
-#           for chunk in pdfResponse.iter_content(100000):
-#               pdfFile.write(chunk)
-#           pdfFile.close()
-#           i = i + 1
+        print('\nProcessing...')
+        convert_to_mp3(selected_video.title, save_path)
+        print('\nDownload complete')
